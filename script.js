192481e9f814e3c24c2781e9598856b1a234feb4
itpark <a.m.smyslov@yandex.ru>
2023-03-06 20:06:13 +0300

    done servlets

--- script.js
+++ script.js
@@ -27,11 +27,54 @@ function showUsers(users) {
     tableUsersDiv.innerHTML = html;
 }
 
-window.onload = async function () {
+async function loadAndShowUsers() {
     let response = await fetch("http://localhost:8080/users");
 
     if (response.ok) {
         let users = await response.json();
         showUsers(users);
     }
-};
+}
+
+async function saveToServer(user) {
+    let response = await fetch("http://localhost:8080/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json;charset=UTF-8"
+        },
+        body: JSON.stringify(user)
+    });
+}
+
+async function buttonAddUser_Click() {
+    let inputNicknameField = document.getElementById("input-nickname-field");
+    let inputTicketPriceField = document.getElementById("input-ticket_price-field");
+
+    let nickname = inputNicknameField.value.toString();
+    let ticketPrice = inputTicketPriceField.value.toString();
+
+    if (nickname === "") {
+        alert("Вы не ввели никнейм");
+        return;
+    }
+
+    if (ticketPrice === "") {
+        alert("Вы не ввели стоимость билета");
+        return;
+    }
+
+    let user = {
+        id: 0,
+        nickname: nickname,
+        ticketPrice: ticketPrice
+    };
+
+    await saveToServer(user);
+
+    inputNicknameField.value = "";
+    inputTicketPriceField.value = "";
+
+    await loadAndShowUsers();
+}
+
+window.onload = loadAndShowUsers;
